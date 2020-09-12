6115abbdac37793099269dd56ae31a7a51810519
yanghuijin1214 <yangjinee@naver.com>
2020-09-12 20:00:59 +0900

    Updates

---
+++ precache-manifest.c8fd19cb46c426cdb1c3f539fa3aa0b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a6db09df56c9589ff3bddc236893b1",
+    "url": "/navlix/index.html"
+  },
+  {
+    "revision": "60b3df6b7dedd0c8e503",
+    "url": "/navlix/static/css/main.c8648388.chunk.css"
+  },
+  {
+    "revision": "a7734e75b2d5e4931a91",
+    "url": "/navlix/static/js/2.8348e4f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navlix/static/js/2.8348e4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b3df6b7dedd0c8e503",
+    "url": "/navlix/static/js/main.c4e5d831.chunk.js"
+  },
+  {
+    "revision": "9f186743af337180e467",
+    "url": "/navlix/static/js/runtime-main.b89a3882.js"
+  },
+  {
+    "revision": "1c1bce4f3cc22269902370e90bee8e38",
+    "url": "/navlix/static/media/addimage1.1c1bce4f.png"
+  },
+  {
+    "revision": "da0b982c788a3c9cbe9c930bc4e54a6e",
+    "url": "/navlix/static/media/addimage2.da0b982c.png"
+  },
+  {
+    "revision": "9e3e015123b9bd44819d846d06d1f889",
+    "url": "/navlix/static/media/addimage3.9e3e0151.png"
+  }
+]);
